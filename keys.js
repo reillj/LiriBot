401b5d8f8c6408648c0cdd03b25e29cc56bfa91f
reillj <jenniferreilly1992@gmail.com>
2017-10-25 19:15:07 -0700

    Add files via upload

---
+++ keys.js
@@ -0,0 +1,12 @@
+console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: 'kbM1rNG4Dv08PYImuk6amOwkN',
+  consumer_secret: 'uQFZ51FgdhuAUW3LboMUyuhZd5K4pq2K8N81Q9QBiguMuiB036
+',
+  access_token_key: '915715845527027712-GHfTrgY8qRaxDJHGMdXWK1ZPThBFjpD',
+  access_token_secret: 'PxKn74T3NzsOusV6C1C6bRWQdHOjjeg5Yhqkl6k4VM8SL
+',
+}
+
+module.exports = twitterKeys;
